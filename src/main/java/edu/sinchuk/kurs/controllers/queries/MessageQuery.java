e05584d1670d966dad51f8be325ed72129480cb5
OlyaSinch <olyaliveitup@gmail.com>
2017-01-08 18:31:03 +0200

    Add message queries entity

---
+++ src/main/java/edu/sinchuk/kurs/controllers/queries/MessageQuery.java
@@ -0,0 +1,110 @@
+package edu.sinchuk.kurs.controllers.queries;
+
+import edu.sinchuk.kurs.models.entities.MessageEntity;
+import edu.sinchuk.kurs.models.entities.OrderEntity;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 25.12.2016.
+ */
+public class MessageQuery {
+
+    static Connection conn = null;
+    static Statement statement = null;
+
+    public MessageQuery(Connection connection) {
+        this.conn = connection;
+    }
+
+    public void insertMessage(String text, int fkSenderId, int fkReceiverId) throws SQLException {
+        statement = conn.createStatement();
+        String insertMessageSQL = "INSERT INTO message_table"
+                + "(message_id, message_text, fk_sender_id, fk_reciever_id) " + "VALUES"
+                + "(message_sequence.NEXTVAL,'" + text + "'," + fkSenderId + "," + fkReceiverId + ")";
+        statement.executeUpdate(insertMessageSQL);
+        conn.close();
+    }
+
+    public List<MessageEntity> selectAll() throws SQLException {
+        List<MessageEntity> messages = new ArrayList<>();
+        String selectOrderSQL = "SELECT * "
+                + "FROM message_table";
+        PreparedStatement prst = conn.prepareStatement(selectOrderSQL);
+        ResultSet rs = prst.executeQuery();
+
+        MessageEntity message;
+        while (rs.next()) {
+            message = new MessageEntity();
+            message.setMessageId(rs.getInt(1));
+            message.setMessageText(rs.getString(2));
+            message.setFkSenderId(rs.getInt(3));
+            message.setFkReceiverId(rs.getInt(4));
+            messages.add(message);
+        }
+
+        return messages;
+    }
+
+    public List<MessageEntity> selectBySender(int fkSenderId) throws SQLException {
+        List<MessageEntity> messages = new ArrayList<>();
+        String selectOrderSQL = "SELECT * "
+                + "FROM message_table WHERE fk_sender_id = " + fkSenderId;
+        PreparedStatement prst = conn.prepareStatement(selectOrderSQL);
+        ResultSet rs = prst.executeQuery();
+
+        MessageEntity message;
+        while (rs.next()) {
+            message = new MessageEntity();
+            message.setMessageId(rs.getInt(1));
+            message.setMessageText(rs.getString(2));
+            message.setFkSenderId(rs.getInt(3));
+            message.setFkReceiverId(rs.getInt(4));
+            messages.add(message);
+        }
+
+        return messages;
+    }
+
+    public List<MessageEntity> selectByReciever(int fkReceiverId) throws SQLException {
+        List<MessageEntity> messages = new ArrayList<>();
+        String selectOrderSQL = "SELECT * "
+                + "FROM message_table WHERE fk_reciever_id = " + fkReceiverId;
+        PreparedStatement prst = conn.prepareStatement(selectOrderSQL);
+        ResultSet rs = prst.executeQuery();
+
+        MessageEntity message;
+        while (rs.next()) {
+            message = new MessageEntity();
+            message.setMessageId(rs.getInt(1));
+            message.setMessageText(rs.getString(2));
+            message.setFkSenderId(rs.getInt(3));
+            message.setFkReceiverId(rs.getInt(4));
+            messages.add(message);
+        }
+
+        return messages;
+    }
+
+    public List<MessageEntity> selectBySenderAndReciever(int fkSenderId, int fkReceiverId) throws SQLException {
+        List<MessageEntity> messages = new ArrayList<>();
+        String selectOrderSQL = "SELECT * "
+                + "FROM message_table WHERE fk_sender_id = " + fkSenderId + " AND fk_reciever_id = " + fkReceiverId;
+        PreparedStatement prst = conn.prepareStatement(selectOrderSQL);
+        ResultSet rs = prst.executeQuery();
+
+        MessageEntity message;
+        while (rs.next()) {
+            message = new MessageEntity();
+            message.setMessageId(rs.getInt(1));
+            message.setMessageText(rs.getString(2));
+            message.setFkSenderId(rs.getInt(3));
+            message.setFkReceiverId(rs.getInt(4));
+            messages.add(message);
+        }
+
+        return messages;
+    }
+}
